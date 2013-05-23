605fc0ba99b0a35ab44ab0d56d502e0b83dbe440
matteo <gfgullo@live.com>
2013-05-23 23:10:12 +0200

    camera

--- Android app/gen/poli/app/ulisse/R.java
+++ Android app/gen/poli/app/ulisse/R.java
@@ -195,21 +195,23 @@ containing a value of this type.
         public static final int torre_di_pisa=0x7f02001d;
     }
     public static final class id {
-        public static final int BtnSlide=0x7f060009;
-        public static final int ImageView=0x7f060006;
-        public static final int action_settings=0x7f06000e;
+        public static final int BtnSlide=0x7f06000b;
+        public static final int action_settings=0x7f060010;
+        public static final int camera_surface=0x7f060005;
+        public static final int camera_surface_button=0x7f060007;
+        public static final int camera_surface_footer=0x7f060006;
         public static final int hybrid=0x7f060004;
-        public static final int list=0x7f06000b;
-        public static final int map=0x7f060008;
-        public static final int menu=0x7f06000a;
-        public static final int menu_settings=0x7f06000d;
+        public static final int list=0x7f06000d;
+        public static final int map=0x7f06000a;
+        public static final int menu=0x7f06000c;
+        public static final int menu_settings=0x7f06000f;
         public static final int none=0x7f060000;
         public static final int normal=0x7f060001;
         public static final int satellite=0x7f060002;
-        public static final int sign_in_button=0x7f060007;
+        public static final int sign_in_button=0x7f060009;
         public static final int terrain=0x7f060003;
-        public static final int textView1=0x7f060005;
-        public static final int top=0x7f06000c;
+        public static final int textView1=0x7f060008;
+        public static final int top=0x7f06000e;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
