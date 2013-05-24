62169e1063f5f22054abfa7c48ac85a1616134bd
matteo <gfgullo@live.com>
2013-05-24 08:38:41 +0200

    Fixed page activity

--- Android app/src/poli/app/ulisse/PageActivity.java
+++ Android app/src/poli/app/ulisse/PageActivity.java
@@ -1,12 +1,25 @@
 package poli.app.ulisse;
 
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PageActivity extends Activity implements OnClickListener
 {
@@ -18,15 +31,31 @@ public class PageActivity extends Activity implements OnClickListener
 	 super.onCreate(savedInstanceState);
      setContentView(R.layout.page_activity);
      findViewById(R.id.btStreetview).setOnClickListener(this);
-     b = getIntent().getExtras();
-     TextView tvTitle=(TextView)findViewById(R.id.tvTitle);
-     TextView tvAddr=(TextView)findViewById(R.id.tvAddr);
-
-     tvTitle.setText(b.getString("title"));
-     tvAddr.setText(b.getString("addr"));
-
+     String place_id = getIntent().getExtras().getString("id");
+     Communication com=new Communication();
+     
+     b=com.getPlace(place_id);
+     try {
+		this.createUI();
+     } catch (JSONException e) {
+		e.printStackTrace();
+     }
+ 
     }
 
+    public void createUI() throws JSONException {
+    	((TextView)findViewById(R.id.tvTitle)).setText(b.getString("name"));
+    	((TextView)findViewById(R.id.tvAddr)).setText(b.getString("address"));
+    	ArrayList<Object> array = (ArrayList<Object>)b.get("documents");
+		((TextView)findViewById(R.id.TextView0)).setText(((JSONObject)array.get(0)).getString("name"));
+		((TextView)findViewById(R.id.TextView1)).setText(((JSONObject)array.get(1)).getString("name"));
+		((TextView)findViewById(R.id.TextView2)).setText(((JSONObject)array.get(2)).getString("name"));
+		((TextView)findViewById(R.id.TextView3)).setText(((JSONObject)array.get(3)).getString("name"));
+		((TextView)findViewById(R.id.TextView4)).setText(((JSONObject)array.get(4)).getString("name"));
+		((TextView)findViewById(R.id.TextView5)).setText(((JSONObject)array.get(5)).getString("name"));
+
+    }
+    
 	@Override
 	public void onClick(View arg0) 
 	{
