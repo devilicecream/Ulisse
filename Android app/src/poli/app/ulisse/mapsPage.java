246a4dea0dcc2af985dfef0f56767ee99ebb5653
davide <davidep93@alice.it>
2013-05-24 03:02:26 +0200

    menu2

--- Android app/src/poli/app/ulisse/mapsPage.java
+++ Android app/src/poli/app/ulisse/mapsPage.java
@@ -1,6 +1,7 @@
 package poli.app.ulisse;
-
 import android.support.v4.app.Fragment;
+import android.widget.SearchView;
+import com.google.android.gms.maps.MapFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +15,9 @@ import android.widget.LinearLayout;
  * Time: 15.23
  * To change this template use File | Settings | File Templates.
  */
-public class mapsPage extends Fragment {
+public class mapsPage extends Fragment implements View.OnClickListener {
+
+    SearchView search;
 
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
 
@@ -23,8 +26,27 @@ public class mapsPage extends Fragment {
             return null;
         }
         // inflate view from layout
-        View view = (LinearLayout)inflater.inflate(R.layout.left,container,false);
-
+        View view = inflater.inflate(R.layout.left,container,false);
+        search=(SearchView)view.findViewById(R.id.searchView);
+        search.setOnSearchClickListener(this);
         return view;
     }
+
+
+    public void onBtnClicked()
+    {
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case(R.id.searchView):
+                String res=search.getQuery().toString();
+        }
+    }
 }
+
+
